3604818d185bef761c81d5bb390009200051c19e
man-want-fly <man.want.fly@gmail.com>
2025-12-08 20:17:33 +0800

    Prepare binaries for 1.0.0

--- Package.swift
+++ Package.swift
@@ -1,26 +1,29 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "JPushSDK",
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "JPushSDK",
             targets: ["JPushSDK"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "JPushSDK"
-        ),
-        .testTarget(
-            name: "JPushSDKTests",
-            dependencies: ["JPushSDK"]
-        ),
+        .binaryTarget(
+            name: "jcore-ios-5.2.2",
+            url: "https://github.com/man-want-fly/JPushSDK/releases/download/1.0.0/jcore-ios-5.2.2.xcframework.zip",
+            checksum: "e0aaff4800418d60c5b77dfaf3b871089908bb84cc82e5fc80f40702cf295587"
+        )
+        .binaryTarget(
+            name: "jpush-ios-5.9.0",
+            url: "https://github.com/man-want-fly/JPushSDK/releases/download/1.0.0/jpush-ios-5.9.0.xcframework.zip",
+            checksum: "beddbab28822329e0afe7e0a3548d04e0f03d2442690eac5b4675249bf7ee824"
+        )
     ]
 )
---
+++ Sources/JPushSDK/JPushSDK.swift
@@ -0,0 +1,6 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@_exported import jcore-ios-5.2.2
+@_exported import jpush-ios-5.9.0
+
